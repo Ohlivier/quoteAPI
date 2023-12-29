1d26fabfafd696659bfaeeec86b2c33496320370
Olivie <olivierrustige@gmail.com>
2023-12-29 12:47:03 +0100

    switch from sqlite3 to sqlalchemy (actually this time)

--- main.py
+++ main.py
@@ -1,25 +1,45 @@
 from fastapi import FastAPI, Request
-import sqlite3
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+from sqlalchemy.sql.expression import func
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
-with sqlite3.connect("quotes.db") as connection:
-    cursor = connection.cursor()
+engine = create_engine('sqlite:///quotes.db', echo=True)
+conn = engine.connect()
+meta = MetaData()
+
+quotes = Table(
+    'quotes', meta,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('quote', String),
+    Column('author', String),
+)
 
 
-def get_random_quote(request=None):
-    rows = cursor.execute("SELECT * FROM quotes ORDER BY RANDOM() LIMIT 1;").fetchall()
+class Quote(BaseModel):
+    id: int
+    quote: str
+    author: str
+
+
+def get_random_quote(request: Request = None):
+    q = quotes.select().order_by(func.random())
+    result = conn.execute(q)
+    rows = result.fetchone()
     if request is None:
-        return {"ID": rows[0][0], "Quote": rows[0][1], "Author": rows[0][2]}
+        return {"ID": rows[0], "Quote": rows[1], "Author": rows[2]}
     else:
-        return {"request": request, "ID": rows[0][0], "Quote": rows[0][1], "Author": rows[0][2]}
+        return {"request": request, "ID": rows[0], "Quote": rows[1], "Author": rows[2]}
 
 
 def get_quote_by_id(quote_id: int):
-    rows = cursor.execute("SELECT * FROM quotes WHERE id = ?", (quote_id,)).fetchall()
+    q = quotes.select().where(quotes.c.id == quote_id)
+    result = conn.execute(q)
+    rows = result.fetchone()
     if rows:
-        return {"ID": rows[0][0], "Quote": rows[0][1], "Author": rows[0][2]}
+        return {"ID": rows[0], "Quote": rows[1], "Author": rows[2]}
     else:
         return {"error": "No quote found with the given ID"}
 
