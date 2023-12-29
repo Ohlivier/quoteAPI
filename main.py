1623be3cffac72b22df68b92a2b04c3de5427e27
Olivie <olivierrustige@gmail.com>
2023-12-29 01:30:41 +0100

    Add quote id

--- main.py
+++ main.py
@@ -11,17 +11,35 @@ with sqlite3.connect("quotes.db") as connection:
 def get_random_quote(request=None):
     rows = cursor.execute("SELECT * FROM quotes ORDER BY RANDOM() LIMIT 1;").fetchall()
     if request is None:
-        return {"Quote": rows[0][0], "Author": rows[0][1]}
+        return {"ID": rows[0][0], "Quote": rows[0][1], "Author": rows[0][2]}
     else:
-        return {"request": request, "Quote": rows[0][0], "Author": rows[0][1]}
+        return {"request": request, "ID": rows[0][0], "Quote": rows[0][1], "Author": rows[0][2]}
 
+
+def get_quote_by_id(quote_id: int):
+    rows = cursor.execute("SELECT * FROM quotes WHERE id = ?", (quote_id,)).fetchall()
+    if rows:
+        return {"ID": rows[0][0], "Quote": rows[0][1], "Author": rows[0][2]}
+    else:
+        return {"error": "No quote found with the given ID"}
+
+
+# @app.get("/quote")
+# async def root():
+#     return get_random_quote()
 
 
 @app.get("/quote")
-async def root():
-    return get_random_quote()
+async def get_quote(id: int = None):
+    if id is None:
+        return get_random_quote()
+    else:
+        return get_quote_by_id(id)
 
 
 @app.get("/")
 async def name(request: Request):
     return templates.TemplateResponse("base.html", get_random_quote(request))
+
+
+print("done")
